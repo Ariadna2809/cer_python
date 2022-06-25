25f59ed2baf25c21b5bb9bf9b4dfe647ad4e843d
“Ariadna2809” <1219100450@alumnos.utng.edu.mx>
2022-06-24 19:24:32 -0500

    Laboratorio Completado

--- basico/modulo4/lab4319.py
+++ basico/modulo4/lab4319.py
@@ -1,3 +1,16 @@
 #Laboratorio 4.3.1.9
 #Elaborado por: Ariadna Loredo
 
+def is_prime(num):
+    div = 2
+    while div<=num:
+        if num % div != 0:
+            div+=1
+            return num
+        else:
+            return False
+    
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
